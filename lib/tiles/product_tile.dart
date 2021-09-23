9c5280c014842b4c4f4de4444db1386e6ac156b4
isacmaximo <isacmaximomoura@gmail.com>
2021-09-23 08:41:35 -0300

    Criando a Tile da Grid

--- lib/tiles/product_tile.dart
+++ lib/tiles/product_tile.dart
@@ -1,16 +1,61 @@
+//Configuração das vizualizações dos produtos (Grade ou Lista)
 
 import 'package:flutter/material.dart';
 import 'package:loja_virtual/datas/product_data.dart';
 
+
+//O ProductData contém todos os campos sobre o produto
 class ProductTile extends StatelessWidget {
 
   final String type;
-  final ProductData data;
+  final ProductData product;
 
-  ProductTile(this.data, this.type);
+  ProductTile(this.type, this.product);
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    //o InkWell diferentemente do Gesture Detector, tem uma animação melhor ao tocar no widget
+    return InkWell(
+      child: Card(
+
+        //se o tipo for grade então retorna uma coluna , se não retorna uma linha
+        //stretch quer dizer que vai ficar esticado (imagens no caso)
+        //altura / largura = aspect ratio
+        child: type == "grid" ? Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+
+            //Pegando a primeira imagem para exibir:
+            AspectRatio(
+              aspectRatio: 0.8,
+              child: Image.network(product.images[0], fit: BoxFit.cover,),
+            ),
+
+            //Título e Preço:
+            Expanded(
+              child: Container(
+                padding: EdgeInsets.all(8.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(product.title, style: TextStyle(fontWeight: FontWeight.w500),),
+                    Text("R\$ ${product.price.toStringAsFixed(2)}",
+                      style: TextStyle(
+                        color: Theme.of(context).primaryColor,
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.bold
+                      ),),
+                  ],
+                ),
+              ),
+            )
+          ],
+
+        ) : Row(
+
+        ),
+
+      ),
+    );
   }
 }
